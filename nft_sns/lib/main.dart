5d0fa5c82fc6d169aae9c806fa50f99ddc89885a
Haruqun <info@fivenine-design.com>
2022-06-08 03:05:05 +0900

    init

--- nft_sns/lib/main.dart
+++ nft_sns/lib/main.dart
@@ -33,7 +33,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  String _time = '00:00:00';
 
   void _incrementCounter() {
     setState(() {
